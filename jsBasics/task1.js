3258dec92539474dceeb2bb24c875a668213fa90
Olha Shevchenko <olhashe@Olhas-MacBook-Pro.local>
2025-09-28 16:46:25 +0300

    ДЗ 4.1. JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,23 @@
+// Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: 
+// рядок, число, буль, null та undefined. 
+// Присвойте їм значення та виведіть кожну змінну на консоль.
+
+// Рядок (String)
+const myString = "hello world!";
+console.log("String:", myString);
+
+// Число (Number)
+const myNumber = 42;
+console.log("Number:", myNumber);
+
+// Булеве значення (Boolean)
+const isBoolean = true;
+console.log("Boolean:", isBoolean);
+
+// Значення null
+const nullValue = null;
+console.log("Null:", nullValue);
+
+// Значення undefined
+let undefinedValue;
+console.log("Undefined:", undefinedValue);
