92043ca0e1e8de6e0604082717fd410fb15949b5
Olha Shevchenko <olhashe@Olhas-MacBook-Pro.local>
2025-10-05 09:23:55 +0300

    part-9 Objects in JavaScript

---
+++ part-9/task1.js
@@ -0,0 +1,6 @@
+const student = {}
+student.firstName = "Анна"
+student.lastName = "Федоренко"
+student.age = 30
+student.courses = ["Математика", "Історія", "Програмування"]
+console.log(student)
---
+++ part-9/task2.js
@@ -0,0 +1,9 @@
+const book = {
+  title: 'JS is awesome',
+  author: 'Брендан Айх',
+  year: 2000
+}
+
+const { title, author } = book
+console.log('title=', title)
+console.log('author=', author)
